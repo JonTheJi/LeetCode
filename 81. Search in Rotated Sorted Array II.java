d6b58d573f6395828baa258b924bc3fdce8d8748
JonTheJi <johnjiangtw0804@gmail.com>
2018-09-27 02:03:57 -0700

    Remember for the case that nums.length < 3 use either or for binary seach since we want it to keep searching. (search the sorted part, and when we meet duplicates remember to start++ or end -- (dependes on what conditions you use to test the *sorted side you will know what mid equals to, and since you already tested that nums[mid] != target simply remove that end/start) => keep searching and remove the duplicate

---
+++ 81. Search in Rotated Sorted Array II.java
@@ -0,0 +1,35 @@
+/* remember to use either this or this for BS searching condition because nums[mid] would naturally == nums[start]
+   when nums.length < 3
+*/
+public boolean search(int[] nums, int target) {
+        class Solution {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid <= nums.length  - 1 && mid >= 0 && nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] < nums[end]) {
+                if (nums[mid] < target && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else if (nums[mid] > nums[end]) {
+                if (nums[mid] > target && target >= nums[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                end--;
+            }
+        }
+        return false;
+    }
+}
